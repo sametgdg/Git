5a71718e3dd87ce40b206dac1a80ac6498a6eb52
sametgdg <samet.gdg@gmail.com>
2016-11-12 04:30:11 +0200

    Initial commit

---
+++ Git/app/src/main/java/com/sametgdg/sametgdg/git/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sametgdg.sametgdg.git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    String a ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        topla();
+    }
+
+    void topla(){
+
+    }
+}
