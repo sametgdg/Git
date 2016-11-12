4a0dea7eaab559c0e0ed40f5cbbbb7f07a21f6d1
sametgdg <samet.gdg@gmail.com>
2016-11-12 04:49:20 +0200

    sonn

--- Git/app/src/main/java/com/sametgdg/sametgdg/git/MainActivity.java
+++ Git/app/src/main/java/com/sametgdg/sametgdg/git/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     String a ;
+    String d ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
